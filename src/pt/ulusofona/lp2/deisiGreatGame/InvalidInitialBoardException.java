97feed6d4bffbc3b7649cb2a0c6050370042ae0d
goncaloantunes22006713 <a22006713@alunos.ulusofona.pt>
2022-01-10 15:33:13 +0000

    InvalidInitialBoardException

--- src/pt/ulusofona/lp2/deisiGreatGame/InvalidInitialBoardException.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/InvalidInitialBoardException.java
@@ -1,24 +1,34 @@
 package pt.ulusofona.lp2.deisiGreatGame;
 
- public class InvalidInitialBoardException extends Exception {
+public class InvalidInitialBoardException extends Exception {
 
     private String message;
+    private boolean isInvalidAbyss;
+    private boolean isInvalidTool;
+    private String typeID;
     InvalidInitialBoardException(String message) {
         this.message = message;
+    }
+
+    public InvalidInitialBoardException(String message, boolean isInvalidAbyss, boolean isInvalidTool, String typeID) {
+        this.message = message;
+        this.isInvalidAbyss = isInvalidAbyss;
+        this.isInvalidTool = isInvalidTool;
+        this.typeID = typeID;
     }
 
     public String getMessage(){
         return message;
     }
 
-     public Boolean isInvalidAbyss(){
-        return true;
+    public Boolean isInvalidAbyss(){
+        return isInvalidAbyss;
     }
 
-     public Boolean isInvalidTool(){
-        return true;
+    public Boolean isInvalidTool(){
+        return isInvalidTool;
     }
-     public String getTypeId(){
-        return "";
+    public String getTypeId(){
+        return typeID;
     }
 }

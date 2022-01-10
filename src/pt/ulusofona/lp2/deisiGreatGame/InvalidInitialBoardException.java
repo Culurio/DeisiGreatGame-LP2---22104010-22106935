f13f3ec54f389623dc283145d73d65c0ca040613
goncaloantunes22006713 <a22006713@alunos.ulusofona.pt>
2022-01-10 15:35:09 +0000

    InvalidInitialBoardExceptio melhor

--- src/pt/ulusofona/lp2/deisiGreatGame/InvalidInitialBoardException.java
+++ src/pt/ulusofona/lp2/deisiGreatGame/InvalidInitialBoardException.java
@@ -4,17 +4,35 @@ public class InvalidInitialBoardException extends Exception {
 
     private String message;
     private boolean isInvalidAbyss;
+    private boolean isEffectType;
+    private boolean isEffectID;
     private boolean isInvalidTool;
+    private boolean isSizeInvalid;
+    private boolean isNameInvalid;
+    private boolean isColorInvalid;
+    private boolean isPlayerIdInvalid;
     private String typeID;
     InvalidInitialBoardException(String message) {
         this.message = message;
     }
 
-    public InvalidInitialBoardException(String message, boolean isInvalidAbyss, boolean isInvalidTool, String typeID) {
+    public InvalidInitialBoardException(String message, boolean isInvalidAbyss, boolean isInvalidTool,
+                                        boolean isEffectType,boolean isEffectID, String typeID) {
         this.message = message;
         this.isInvalidAbyss = isInvalidAbyss;
         this.isInvalidTool = isInvalidTool;
+        this.isEffectType = isEffectType;
+        this.isEffectID = isEffectID;
         this.typeID = typeID;
+    }
+
+    public InvalidInitialBoardException(String message, boolean isSizeInvalid, boolean isNameInvalid,
+                                        boolean isColorInvalid,boolean isPlayerIdInvalid) {
+        this.message = message;
+        this.isSizeInvalid = isSizeInvalid;
+        this.isNameInvalid = isNameInvalid;
+        this.isColorInvalid = isColorInvalid;
+        this.isPlayerIdInvalid = isPlayerIdInvalid;
     }
 
     public String getMessage(){
@@ -23,6 +41,26 @@ public class InvalidInitialBoardException extends Exception {
 
     public Boolean isInvalidAbyss(){
         return isInvalidAbyss;
+    }
+
+    public boolean isSizeInvalid() {
+        return isSizeInvalid;
+    }
+
+    public boolean isNameInvalid() {
+        return isNameInvalid;
+    }
+
+    public boolean isColorInvalid() {
+        return isColorInvalid;
+    }
+
+    public boolean isPlayerIdInvalid() {
+        return isPlayerIdInvalid;
+    }
+
+    public String getTypeID() {
+        return typeID;
     }
 
     public Boolean isInvalidTool(){
